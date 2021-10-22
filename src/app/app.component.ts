bdb50f367dbed4efc0f976b9510e22fc3adf15a4
HADASISRAELI\hadas <hadas25496@gmail.com>
2021-10-22 19:18:14 +0300

    adding timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Timer';
+  // timeSec: number = 0;
+  interval: any;
+  time = new Date(+0);
+  saveTime: any;
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      this.time.setSeconds(this.time.getSeconds() + 1);
+
+      // if(this.timeSec < 60) {
+      //   this.timeSec++;
+      // } else {
+      //   this.timeSec = 0;
+      // }
+    }, 1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+  resetTimer() {
+    this.time.setSeconds(0);
+    this.time.setMinutes(0);
+    // this.time.setHours(0);
+  }
+
+  stopTimer() {
+
+    this.saveTime.getTime(this.interval);
+  }
 }
