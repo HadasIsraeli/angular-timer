7da170ffbc6c8bf1a1f9c2559d4d3240bdfa7e69
HADASISRAELI\hadas <hadas25496@gmail.com>
2021-10-22 16:23:34 +0300

    starting the timer app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CdTimerModule } from 'angular-cd-timer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CdTimerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
