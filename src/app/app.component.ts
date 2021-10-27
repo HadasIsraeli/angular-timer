aefc121656504a8ddd6e003f2df306f5da34cbe0
HADASISRAELI\hadas <hadas25496@gmail.com>
2021-10-27 19:13:25 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,10 +45,6 @@ export class AppComponent {
     this.timerName = 'Timer';
   }
 
-  addTimer() {
-
-  }
-
   clearHistory() {
     this.history = [];
   }
@@ -58,16 +54,10 @@ export class AppComponent {
   }
 
   exportexcel(): void {
-    /* pass here the table id */
     let element = document.getElementById('excel-table');
     const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
-
-    /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-    /* save to file */
     XLSX.writeFile(wb, this.fileName);
-
   }
 }
